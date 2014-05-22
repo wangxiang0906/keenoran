ee89b3bbb1b67f09815fc02d0b2732e64155536a
wangxiang0906 <wangxiang0906@gmail.com>
2014-05-22 18:28:00 +0800

    第一句java代码

---
+++ keenoran/src/com/keen/test/Test1.java
@@ -0,0 +1,16 @@
+package com.keen.test;
+
+/**
+ * 
+ * @ClassName: Test1 
+ * @Description: TODO
+ * @author 王翔   wangxiang0906@foxmail.com
+ * @date 2014年5月22日 下午6:27:19 
+ *
+ */
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Hello world...");
+	}
+
+}
